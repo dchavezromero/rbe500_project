245afd63a10ff874b34e10b0de2293bff9d91c02
Dennis Chavez <dennis.chavezromero@hotmail.com>
2021-04-02 05:25:37 -0400

    Added functionality to write joint values based on ee pose from inv kin server node

--- scripts/inverse_kinematics_client.py
+++ scripts/inverse_kinematics_client.py
@@ -10,8 +10,8 @@ def calc_inv_kin_client(x, y, z):
     rospy.wait_for_service('inverse_kinematics')
     try:
         inverse_kinematics = rospy.ServiceProxy('inverse_kinematics', InvKin)
-        resp1 = inverse_kinematics(x, y, z)
-        return resp1.joint_vals
+        angles = inverse_kinematics(x, y, z)
+        return angles
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
 
@@ -19,6 +19,7 @@ def usage():
     return "%s [x y z]"%sys.argv[0]
 
 if __name__ == "__main__":
+    
     if len(sys.argv) == 4:
         x = int(sys.argv[1])
         y = int(sys.argv[2])
@@ -27,5 +28,6 @@ if __name__ == "__main__":
         print(usage())
         sys.exit(1)
 
-    print("Requesting %s,%s,%s"%(x, y, z))
-    print(calc_inv_kin_client(x, y, z))
+    angle_vals = calc_inv_kin_client(x, y, z)
+
+    print(angle_vals.joint_vals)
--- scripts/inverse_kinematics_server.py
+++ scripts/inverse_kinematics_server.py
@@ -5,13 +5,22 @@ from __future__ import print_function
 from rbe500_project.srv import InvKin,InvKinResponse
 from rbe500_project.msg import joint_angles
 import rospy
+from std_msgs.msg import Float64
 
+pub_theta1 = rospy.Publisher("/scara/theta1_position_controller/command", Float64, queue_size=1)
+pub_theta2 = rospy.Publisher("/scara/theta1_position_controller/command", Float64, queue_size=1)
+pub_d3 = rospy.Publisher("/scara/theta1_position_controller/command", Float64, queue_size=1)
 result = joint_angles()
 
 def calc_inv_kin(req):
     result.theta1 = req.x
     result.theta2 = req.y
     result.d3 = req.z
+
+    pub_theta1.publish(result.theta1)
+    pub_theta2.publish(result.theta2)
+    pub_d3.publish(result.d3)
+
     return InvKinResponse(result)
 
 def calc_inv_kin_server():
