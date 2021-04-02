762c65fbf2fda23f9edcdf8a3b91e7ad22a31f9c
Dennis Chavez <dennis.chavezromero@hotmail.com>
2021-04-02 17:45:34 -0400

    Documentation run

--- scripts/inverse_kinematics_server.py
+++ scripts/inverse_kinematics_server.py
@@ -22,28 +22,16 @@ def calc_inv_kin(req):
 
     L = 1
 
-    #D2 = -(2*L^2 -(x^2+y^2))/(2*L^2);
     D2 = -(2*L**2 -(x**2 + y**2))/(2*(L**2))
-
-    #C2 = sqrt(1-D2^2);
     C2 = math.sqrt(1-(D2**2))
 
-    #theta2 = atan2d(C2, D2);
     theta2 = math.atan2(C2, D2)
     
-    #alpha = atan2d(y,x);
     alpha = math.atan2(y,x)
-
-    #D1 = (x^2+y^2)/(2*L*sqrt(x^2+y^2));
     D1 = (x**2+y**2)/(2*L*math.sqrt(x**2+y**2))
-
-    #C1 = sqrt(1-D1^2);
     C1 = math.sqrt(1-(D1**2))
-
-    #beta = atan2d(C1, D1);
     beta = math.atan2(C1, D1)
     
-    #theta1 = alpha - beta;
     theta1 = alpha - beta
 
     d3 = L - z
@@ -52,6 +40,7 @@ def calc_inv_kin(req):
     result.theta2 = theta2
     result.d3 = d3
 
+    # Move joints to calculated values
     pub_theta1.publish(result.theta1)
     pub_theta2.publish(result.theta2)
     pub_d3.publish(result.d3)
